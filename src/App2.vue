7de52c6664c73935ebcf593c1d276d87897b7188
Yoshihiro Ishizaka <yoshihiro.twenty@gmail.com>
2024-02-14 08:49:05 +0000

    create vue app

---
+++ src/App2.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+    <div id="app">
+        <button @click="count++">Count is: {{ count }}</button>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import App2 from './App2.vue'
+
+createApp(App2).mount('#app')
